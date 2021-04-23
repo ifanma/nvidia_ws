1f9132e57740b5a88569f993384d3f70960aa41b
ifanma <1337875013@qq.com>
2021-04-23 12:55:20 +0800

    机器人整机状态反馈

--- src/rob_feedback/src/rob_feedback_pub.cpp
+++ src/rob_feedback/src/rob_feedback_pub.cpp
@@ -3,6 +3,7 @@
 #include "std_msgs/Float32.h"
 #include "sensor_msgs/JointState.h"
 #include "geometry_msgs/WrenchStamped.h"
+#include "serial_dev_msgs/systemState.h"
 
 #include <boost/algorithm/string/classification.hpp>
 #include <boost/algorithm/string/split.hpp>
@@ -80,7 +81,7 @@ int main(int argc, char *argv[])
 	ros::Publisher js_pub = n.advertise<sensor_msgs::JointState>("joint_states", 1000);
 	ros::Publisher wrc_l_pub = n.advertise<geometry_msgs::WrenchStamped>("wrench_left", 1000);
 	ros::Publisher wrc_r_pub = n.advertise<geometry_msgs::WrenchStamped>("wrench_right", 1000);
-	ros::Publisher vm = n.advertise<std_msgs::Float32>("voltage_mesr", 1000);
+	ros::Publisher ss_pub = n.advertise<serial_dev_msgs::systemState>("system_state", 1000);
 	ros::Rate loop_rate(1000);
 
 	int rec_len = 0;
@@ -88,10 +89,11 @@ int main(int argc, char *argv[])
 	std::vector<std::string> vStr;
 	double joint[14];
 	int pub_cnt = 0;
-	int i = 0;
+	int i = 0, j = 0;
 	double ft[12] = {0.0};
+	int lastsize = 0;
 
-	std_msgs::Float32 voltage;
+	serial_dev_msgs::systemState ss;
 	
 	while (ros::ok())
 	{
@@ -111,7 +113,9 @@ int main(int argc, char *argv[])
 			try{
 
 				boost::split( vStr, s, boost::is_any_of( "," ), boost::token_compress_on );
-				ROS_INFO_STREAM(vStr.size());
+				if (vStr.size() != lastsize){
+					ROS_INFO_STREAM(vStr.size());
+				}
 				for( i = 0; i < js.name.size(); i++)
 				{
 					js.position.at(i) = atof(vStr.at(i).c_str());
@@ -169,7 +173,42 @@ int main(int argc, char *argv[])
 				w_r.wrench.torque.y = ft[10];
 				w_r.wrench.torque.z = ft[11];
 
-				voltage.data = atof(vStr.at(60).c_str());
+				ss.voltage = atof(vStr.at(60).c_str());
+
+				i = 75;
+				ss.left_motor_state.clear();
+				for (j = 0; j< 7; j++)
+				{
+					ss.left_motor_state.append(atof(vStr.at(i ++).c_str()));
+				}
+				ss.left_arm_state = atof(vStr.at(i ++).c_str());
+				ss.left_arm_fctrl_state = atof(vStr.at(i ++).c_str());
+
+				ss.right_motor_state.clear();
+				for (j = 0; j< 7; j++)
+				{
+					ss.right_motor_state.append(atof(vStr.at(i ++).c_str()));
+				}
+				ss.right_arm_state = atof(vStr.at(i ++).c_str());
+				ss.right_arm_fctrl_state = atof(vStr.at(i ++).c_str());
+
+				ss.leg_motor_state.clear();
+				for (j = 0; j< 5; j++)
+				{
+					ss.leg_motor_state.append(atof(vStr.at(i ++).c_str()));
+				}
+				ss.leg_arm_state = atof(vStr.at(i ++).c_str());
+
+				ss.trc_motor_state.clear();
+				for (j = 0; j< 4; j++)
+				{
+					ss.leg_motor_state.append(atof(vStr.at(i ++).c_str()));
+				}
+				ss.leg_arm_state = atof(vStr.at(i ++).c_str());
+
+				if (i - 75 != 30){
+					ROS_INFO("wrong index");
+				}
 
 				if (pub_cnt > int(1000.0/param_rate))
 				{
@@ -177,12 +216,12 @@ int main(int argc, char *argv[])
 					js_pub.publish(js);
 					wrc_l_pub.publish(w_l);
 					wrc_r_pub.publish(w_r);
-					vm.publish(voltage);
+					ss_pub.publish(ss);
 				}
 				pub_cnt ++;
 			}
 			catch(std::exception e1){
-				ROS_WARN("failed\n");
+				ROS_WARN("deal failed\n");
 				// ROS_INFO_STREAM(e1.what());
 			}	
 		}
